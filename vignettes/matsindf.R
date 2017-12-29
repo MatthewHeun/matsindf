43e80adbe3e3ffaa2c46c158c2be9553ff4d266e
MatthewHeun <matthew.heun@me.com>
2017-12-29 10:29:27 -0500

    Now showing matrices

--- vignettes/matsindf.R
+++ vignettes/matsindf.R
@@ -24,23 +24,65 @@ UKEnergy2000_with_metadata <- UKEnergy2000 %>%
       # Identify any places where our logic is faulty.
       TRUE ~ NA_character_
     ),
+    # Columns for rownames, colnames, rowtypes, and coltypes 
+    # use the matrix name column (UVY) as their key,
+    # together with the knowledge that
+    # U is Product by Industry, 
+    # V is Industry by Product, and 
+    # Y is Product by (final demand) Sector.
     rownames = case_when(
-      # The U matrix is Product by Industry.
       UVY == "U" ~ Product,
-      # The V matrix is Industry by Product.
       UVY == "V" ~ Flow,
-      # The Y matrix is Product by Sector.
       UVY == "Y" ~ Product,
-      TRUE ~ NA_integer_
+      TRUE ~ NA_character_
     ), 
     colnames = case_when(
-      # The U matrix is Product by Industry.
       UVY == "U" ~ Flow,
-      # The V matrix is Industry by Product.
       UVY == "V" ~ Product,
-      # The Y matrix is Product by Sector.
       UVY == "Y" ~ Flow,
-      TRUE ~ NA_integer_
+      TRUE ~ NA_character_
+    ), 
+    rowtypes = case_when(
+      UVY == "U" ~ "Product",
+      UVY == "V" ~ "Industry",
+      UVY == "Y" ~ "Product",
+      TRUE ~ NA_character_
+    ), 
+    coltypes = case_when(
+      UVY == "U" ~ "Industry",
+      UVY == "V" ~ "Product",
+      UVY == "Y" ~ "Sector",
+      TRUE ~ NA_character_
     )
+  ) %>% 
+  mutate(
+    # Eliminate columns we no longer need
+    Ledger.side = NULL,
+    Flow.aggregation.point = NULL,
+    Flow = NULL,
+    Product = NULL, 
+    # Ensure that all energy values are positive, as required for analysis.
+    E.ktoe = abs(E.ktoe)
   )
+
+## ------------------------------------------------------------------------
+Energy <- UKEnergy2000_with_metadata %>% 
+  group_by(Country, Year, UVY) %>% 
+  collapse_to_matrices(matnames = "UVY", 
+                       rownames = "rownames", colnames = "colnames", 
+                       rowtypes = "rowtypes", coltypes = "coltypes", 
+                       values = "E.ktoe") %>% 
+  rename(matrix.name = UVY, matrix = E.ktoe)
+glimpse(Energy)
+# 
+# To access one of the matrices, try one of these approaches:
+# 
+# The U matrix
+(Energy %>% filter(matrix.name == "U"))[["matrix"]]
+
+# The V matrix
+Energy$matrix[[2]]
+
+# The Y matrix
+Energy$matrix[[3]]
 

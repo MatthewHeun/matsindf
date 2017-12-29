bb0dcde80ab80b7d520167cf6dc98bfa9981c704
MatthewHeun <matthew.heun@me.com>
2017-12-29 11:46:56 -0500

    Adding more sections to vignette

--- vignettes/matsindf.R
+++ vignettes/matsindf.R
@@ -4,7 +4,9 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 library(dplyr)
+library(tidyr)
 library(tibble)
+library(byname)
 library(matsindf)
 
 ## ------------------------------------------------------------------------
@@ -66,23 +68,44 @@ UKEnergy2000_with_metadata <- UKEnergy2000 %>%
   )
 
 ## ------------------------------------------------------------------------
-Energy <- UKEnergy2000_with_metadata %>% 
+EnergyMats_2000 <- UKEnergy2000_with_metadata %>% 
   group_by(Country, Year, UVY) %>% 
   collapse_to_matrices(matnames = "UVY", 
                        rownames = "rownames", colnames = "colnames", 
                        rowtypes = "rowtypes", coltypes = "coltypes", 
                        values = "E.ktoe") %>% 
   rename(matrix.name = UVY, matrix = E.ktoe)
-glimpse(Energy)
-# 
+
+# The remaining columns are Country, Year, matrix.name, and matrix
+glimpse(EnergyMats_2000)
+
 # To access one of the matrices, try one of these approaches:
-# 
-# The U matrix
-(Energy %>% filter(matrix.name == "U"))[["matrix"]]
+(EnergyMats_2000 %>% filter(matrix.name == "U"))[["matrix"]] # The U matrix
 
-# The V matrix
-Energy$matrix[[2]]
+EnergyMats_2000$matrix[[2]] # The V matrix
 
-# The Y matrix
-Energy$matrix[[3]]
+EnergyMats_2000$matrix[[3]] # The Y matrix
+
+## ------------------------------------------------------------------------
+# Create rows for a fictitious country "AB"
+Energy <- EnergyMats_2000 %>% 
+  rbind(EnergyMats_2000 %>% mutate(Country = "AB")) %>% 
+  spread(key = Year, value = matrix) %>% 
+  # Add a column of multipliers to add noise.
+  mutate(
+    mult = runif(nrow(.), min = 0.9, max = 1.1),
+    `2001` = elementproduct_byname(mult, `2000`),
+    mult = NULL
+  ) %>% 
+  gather(key = Year, value = matrix, `2000`, `2001`) %>% 
+  spread(key = matrix.name, value = matrix) %>% 
+  # Check energy balance
+  mutate(
+    W = difference_byname(transpose_byname(V), U),
+    y = rowsums_byname(Y),
+    # Need to set column name and type on y so it can be subtracted from row sums of W
+    err = difference_byname(rowsums_byname(W), 
+                            y %>% setcolnames_byname("Industry") %>% setcoltype("Industry"))
+  )
+Energy %>% select(Year, Country, err) %>% arrange(Year)
 

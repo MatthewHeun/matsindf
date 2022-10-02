ebe552c96b4729c90f42871883309c551706f2de
Matthew Heun <matthew.heun@me.com>
2022-10-02 17:19:07 -0400

    Better docs.

--- R/expand.R
+++ R/expand.R
@@ -1,38 +1,37 @@
-#' Expand a "tidy" data frame with matsindf-style matrices to a "tidy" data frame with each matrix entry as an observation
+#' Expand a `matsindf` data frame
 #'
-#' A data frame with \pkg{matsindf}-style matrices contains matrices with names \code{matnames}
-#' in the column specified by \code{matvals}).
-#' An IO-style matrix has named rows and columns.
-#' In addition, \pkg{matsindf}-style matrices have "types" for row and column information, such as "Commodities", "Industries",
-#' "Products", or "Machines".
+#' Any tidy data frame of matrices (in which each row represents one matrix observation)
+#' can also be represented as a tidy data frame
+#' with each non-zero matrix entry as an observation on its own row.
+#' This function (and `collapse_to_matrices()`) convert between the two representations.
 #'
-#' Names for output columns are specified in the \code{rownames}, \code{colnames},
-#' \code{rowtypes}, and \code{coltypes}, arguments.
-#' The entries of the \pkg{matsindf}-style matrices are stored in an output column named \code{values}.
+#' Names for output columns are specified in the `rownames`, `colnames`,
+#' `rowtypes`, and `coltypes`, arguments.
+#' The entries of the \pkg{matsindf}-style matrices are stored in an output column named `values.`
 #'
-#' @param      .DF the data frame containing \pkg{matsindf}-style matrices.
-#'                 (\code{.DF} may also be a named list of matrices, in which case
+#' @param      .DF The data frame containing \pkg{matsindf}-style matrices.
+#'                 (`.DF` may also be a named list of matrices, in which case
 #'                 names of the matrices are taken from the names of items in the list and
 #'                 list items are expected to be matrices.)
-#' @param matnames name of the column in \code{.DF} containing matrix names (a string).
-#'                 Default is "\code{matnames}".
-#' @param  matvals name of the column in \code{.DF} containing IO-style matrices or constants (a string),
+#' @param matnames The name of the column in `.DF` containing matrix names (a string).
+#'                 Default is "matnames".
+#' @param  matvals The name of the column in `.DF` containing IO-style matrices or constants (a string),
 #'                 This will also be the name of the column containing matrix entries in the output data frame.
-#'                 Default is "\code{matvals}".
-#' @param rownames name for the output column of row names (a string).
-#'                 Default is "\code{rownames}".
-#' @param colnames name for the output column of column names (a string).
-#'                 Default is "\code{colnames}".
-#' @param rowtypes optional name for the output column of row types (a string).
-#'                 Default is "\code{rowtypes}".
-#' @param coltypes optional name for the output column of column types (a string)
-#'                 Default is "\code{coltypes}".
-#' @param     drop if specified, the value to be dropped from output,
-#'                 For example, \code{drop = 0} will cause \code{0} entries in the matrices to be deleted from output.
-#'                 If \code{NA}, no values are dropped from output.
-#'                 Default is \code{NA}.
+#'                 Default is "matvals".
+#' @param rownames The name for the output column of row names (a string).
+#'                 Default is "rownames".
+#' @param colnames The name for the output column of column names (a string).
+#'                 Default is "colnames".
+#' @param rowtypes An optional name for the output column of row types (a string).
+#'                 Default is "rowtypes".
+#' @param coltypes The optional name for the output column of column types (a string)
+#'                 Default is "coltypes".
+#' @param     drop If specified, the value to be dropped from output,
+#'                 For example, `drop = 0` will cause `0` entries in the matrices to be deleted from output.
+#'                 If `NA`, no values are dropped from output.
+#'                 Default is `NA`.
 #'
-#' @return a tidy data frame containing expanded \pkg{matsindf}-style matrices
+#' @return A tidy data frame containing expanded \pkg{matsindf}-style matrices
 #'
 #' @export
 #'
@@ -55,7 +54,7 @@
 #'                                  ptype, ptype, ptype, ptype, NA, NA),
 #'                   coltypes  = c(itype, itype, itype, itype, itype, ptype, ptype,
 #'                                 itype, itype, itype, itype, NA, NA),
-#'                   vals  = c(    11  ,  22,    11 ,   22 ,   23 ,   11 ,   22 ,
+#'                   vals      = c(11  ,  22,    11 ,   22 ,   23 ,   11 ,   22 ,
 #'                                 11 ,   12 ,   11 ,   22,   0.2, 0.3)) %>%
 #'   group_by(Country, Year, matrix)
 #' mats <- collapse_to_matrices(tidy, matnames = "matrix", rownames = "row", colnames = "col",

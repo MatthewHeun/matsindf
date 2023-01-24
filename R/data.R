5478970e8466078cce9b930c022a99d6d92f1c58
Matthew Heun <matthew.heun@me.com>
2023-01-24 15:41:37 -0500

    Change to do link.

--- R/data.R
+++ R/data.R
@@ -22,5 +22,5 @@
 #'   \item{Product}{the energy product involved in this flow}
 #'   \item{E.ktoe}{magnitude of the energy flow in ktoe}
 #' }
-#' @source \url{https://www.see.leeds.ac.uk/fileadmin/Documents/research/sri/workingpapers/sri-wp111.pdf}
+#' @source \url{https://doi.org/10.1016/j.apenergy.2018.05.109}
 "UKEnergy2000"
